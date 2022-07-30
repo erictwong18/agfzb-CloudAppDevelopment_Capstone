f0ff6d9a55ea215cd4d1d85a79dea74ad5446372
erictwong18 <erictwong18@yahoo.com>
2022-07-30 21:47:55 +0000

    Added in views

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='about/', view=views.about, name='index'),
     # path for contact us view
-
+    path(route='contact/', view=views.contact, name='index'),
     # path for registration
 
     # path for login

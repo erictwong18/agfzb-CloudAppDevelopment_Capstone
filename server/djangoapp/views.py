566e31ff6e6bfde3e2a062e4759d55e72d56c8d4
erictwong18 <erictwong18@yahoo.com>
2022-08-14 04:00:18 +0000

    Week 3 activity 3 midpoint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
+from .restapis import get_dealers_from_cf
 import logging
 import json
 
@@ -90,9 +91,14 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://788e45ee.us-south.apigw.appdomain.cloud/api/dealerships"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
